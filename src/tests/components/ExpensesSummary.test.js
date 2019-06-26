d03aef41318bfbf5584db509d427723711b4a4b4
Maximiliano Yepez Andrade <myepez@osfe.gob.mx>
2019-06-26 12:38:57 -0500

    Add expense summary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {ExpensesSummary} from '../../components/ExpensesSummary'; 
+
+test('should correctly render expenses summary with one expense',()=>{
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should correctly render expenses summary with multiple expenses',()=>{
+    const wrapper = shallow(<ExpensesSummary expenseCount={253} expensesTotal={23512340987} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -4,18 +4,18 @@ export default [{
     id: '1',
     description: 'Goma',
     note: '',
-    amount: 1.95,
+    amount: 195,
     createdAt: 0
 }, {
     id: '2',
     description: 'Lapiz',
     note: '',
-    amount: 300,
+    amount: 109500,
     createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
     id: '3',
     description: 'Calculadora',
     note: '',
-    amount: 18.23,
+    amount: 4500,
     createdAt:  moment(0).add(4, 'days').valueOf()
 }]
