64099f7f65e4b35a7cfbb81413321b2e508b5b1a
Maximiliano Yepez Andrade <myepez@osfe.gob.mx>
2019-07-04 11:25:19 -0500

    Ya se subio

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,11 +3,11 @@ import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import thunk from 'redux-thunk';
 
-<<<<<<< HEAD
+
 const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-=======
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
->>>>>>> de3a69f3ec1d8c57c8a719084edbb40bf9d15307
+
 
 export default () => {
   const store = createStore(
@@ -15,12 +15,12 @@ export default () => {
       expenses: expensesReducer,
       filters: filtersReducer
     }),
-<<<<<<< HEAD
-    composeEnhacers(applyMiddleware(thunk))
+
+    composeEnhacers(applyMiddleware(thunk)),
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-=======
+
     composeEnhancers(applyMiddleware(thunk))
->>>>>>> de3a69f3ec1d8c57c8a719084edbb40bf9d15307
+
   );
 
   return store;
--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,33 +5,31 @@ import expenses from '../fixtures/expenses';
 
 let startAddExpense, history, wrapper;
 
-<<<<<<< HEAD
 beforeEach(()=>{
     startAddExpense = jest.fn();
     history = {push: jest.fn()};
     wrapper = shallow(<AddExpensePage startAddExpense={startAddExpense} history={history} />);
-=======
+});
 beforeEach(() => {
   startAddExpense = jest.fn();
   history = { push: jest.fn() };
   wrapper = shallow(<AddExpensePage startAddExpense={startAddExpense} history={history} />);
->>>>>>> de3a69f3ec1d8c57c8a719084edbb40bf9d15307
+
 });
 
 test('should render AddExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-<<<<<<< HEAD
+
 test('should hanlde onSubmit', ()=>{
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
     expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
-=======
+
 test('should handle onSubmit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
   expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
->>>>>>> de3a69f3ec1d8c57c8a719084edbb40bf9d15307
