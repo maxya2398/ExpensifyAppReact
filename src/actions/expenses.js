81bcdbf0efb745cbbac6eb0ced916a0536d51893
Maximiliano Yepez Andrade <myepez@osfe.gob.mx>
2019-07-04 14:22:25 -0500

    Remove Expense de BD

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({id}));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -39,6 +47,8 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+
+//SET_EXPENSES
 export const setExpenses=(expenses)=>({
   type: 'SET_EXPENSES',
   expenses
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { editExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
@@ -9,7 +9,7 @@ export class EditExpensePage extends React.Component {
     this.props.history.push('/');
   };
   onRemove = () => {
-    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.startRemoveExpense({ id: this.props.expense.id });
     this.props.history.push('/');
   };
   render() {
@@ -31,7 +31,46 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
   editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-  removeExpense: (data) => dispatch(removeExpense(data))
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import ExpenseForm from './ExpenseForm';
+// import { editExpense, removeExpense } from '../actions/expenses';
+
+// export class EditExpensePage extends React.Component {
+//   onSubmit = (expense) => {
+//     this.props.editExpense(this.props.expense.id, expense);
+//     this.props.history.push('/');
+//   };
+//   onRemove = () => {
+//     this.props.removeExpense({ id: this.props.expense.id });
+//     this.props.history.push('/');
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <ExpenseForm
+//           expense={this.props.expense}
+//           onSubmit={this.onSubmit}
+//         />
+//         <button onClick={this.onRemove}>Remove</button>
+//       </div>
+//     );
+//   }
+// };
+
+// const mapStateToProps = (state, props) => ({
+//   expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+// });
+
+// const mapDispatchToProps = (dispatch, props) => ({
+//   editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+//   removeExpense: (data) => dispatch(removeExpense(data))
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
