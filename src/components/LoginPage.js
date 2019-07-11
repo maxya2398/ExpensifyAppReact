3d7ea4529f4fa92d5bfc87a07c3f7420e9f7bee9
Maximiliano Yepez Andrade <myepez@osfe.gob.mx>
2019-07-11 14:35:30 -0500

    Algunos css y asi

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import {connect} from 'react-redux';
 import {startLogin} from '../actions/auth';
 
 export const LoginPage = ({startLogin}) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">App de Gastos</h1>
+      <p>Registra todos tus gastos</p>
+      <button className="button" onClick={startLogin}>Iniciar Sesión con Google</button>
+    </div>
   </div>
 );
 
@@ -13,3 +17,13 @@ const mapDispatchToProps = ( dispatch )=>({
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
+
+// export const LoginPage = ({startLogin}) => (
+//   <div className="box-layout">
+//     <div className="box-layout__box">
+//       <h1 className="box-layout__title">App de Gastos</h1>
+//       <p>Registra todos tus gastos</p>
+//       <button onClick={startLogin}>Login</button>
+//     </div>
+//   </div>
+// );
